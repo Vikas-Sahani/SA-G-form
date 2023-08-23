12a686175906e6a05832f21ab403c03691dea5ed
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-08-23 17:01:51 +0530

    react-icons used, some styling is done

--- sa-gform/src/App.js
+++ sa-gform/src/App.js
@@ -1,253 +1,331 @@
-import "./App.css";
-
 //importing css & js of BootStrap
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-const Required = () => {
+import { BsCloudCheck } from "react-icons/bs";
+import { BsExclamationCircle } from "react-icons/bs";
+import { BsUpload } from "react-icons/bs";
+
+import "./App.css";
+import { useState } from "react";
+
+const Required = (props) => {
   return (
     <>
-      <span className="text-danger">*</span>
+      <p className="text-danger mt-2" style={{ fontSize: "14px" }}>
+        <span>
+          <BsExclamationCircle className="me-2" style={{ fontSize: "20px" }} />
+        </span>{" "}
+        <span>{props.errorTxt}</span>
+      </p>
     </>
   );
 };
 
+const handleFocus = () => {};
+
 function App() {
+  const [isMCQ, setIsMCQ] = useState(true);
+
   return (
     <div className="App d-flex justify-content-center ">
-      <div className="w-25 ">
-        <div>
-          <h1>Assignment Task</h1>
+      <div
+        style={{
+          backgroundColor: "transparent",
+          border: "1px solid transparent",
+          width: "590px",
+        }}
+      >
+        <div
+          className="rounded-3 bg-white card p-0 "
+          style={{ border: "1px solid rgb(239, 239, 240)" }}
+        >
+          <div
+            className="p-0  m-0 "
+            style={{
+              height: "10px",
+              background: "#673AB7",
+              border: "0px solid transparent",
+              borderRadius: "8px 8px 0px 0px",
+            }}
+          ></div>
+          <h1 className="px-3 ">Assignment Task</h1>
           <hr />
-          <p>
+          <p className="d-flex justify-content-between px-3 ">
+            <span>
+              {" "}
+              vikassahani1110@gmail.com{" "}
+              <span style={{ color: "blue" }}>Switch accounts</span>
+            </span>
+            <BsCloudCheck />
+          </p>
+          <p className="px-3 ">
             The name and photo associated with your Google Account will be
             recorded when you upload files and submit this form. Your email
             address is not part of your response.
           </p>
           <hr />
-          <p className="text-danger "> Indicates required question</p>
+          <p className="text-danger px-3">* Indicates required question</p>
         </div>
 
         {/* MCQ */}
-        <div className="mb-3 p-3 card mcq">
-          <h5>
-            MCQ <Required />
-          </h5>
-          <label>
-            <input type="radio" />
-            Option 1
+        <div className="card mcq">
+          <h6>
+            MCQ <span className="text-danger">*</span>
+          </h6>
+          <label htmlFor="1stMCQOption">
+            <input type="radio" id="1stMCQOption" name="mcq" />
+            &nbsp; Option 1
           </label>
-          <label>
-            <input type="radio" />
-            Option 2
+          <label htmlFor="2ndMCQOption">
+            <input type="radio" id="2ndMCQOption" name="mcq" />
+            &nbsp; Option 2
           </label>
-          <label>
-            <input type="radio" />
-            Option 3
+          <label htmlFor="3rdMCQOption">
+            <input type="radio" id="3rdMCQOption" name="mcq" />
+            &nbsp; Option 3
           </label>
+          {isMCQ ? "" : <Required errorTxt="This is a required question" />}
         </div>
 
         {/* Checkbox */}
-        <div className="mb-3 p-3 card checkBox">
-          <h5>
-            Checkbox <Required />
-          </h5>
-          <label>
-            <input type="checkBox" />
-            Option 1
+        <div className=" card mcq">
+          <h6>
+            Checkbox <span className="text-danger">*</span>
+          </h6>
+          <label htmlFor="1stMCQOption">
+            <input type="checkbox" id="1stMCQOption" name="mcq" />
+            &nbsp; Option 1
           </label>
-          <label>
-            <input type="checkBox" />
-            Option 2
+          <label htmlFor="2ndMCQOption">
+            <input type="checkbox" id="2ndMCQOption" name="mcq" />
+            &nbsp; Option 2
           </label>
-          <label>
-            <input type="checkBox" />
-            Option 3
+          <label htmlFor="3rdMCQOption">
+            <input type="checkbox" id="3rdMCQOption" name="mcq" />
+            &nbsp; Option 3
           </label>
+          {isMCQ ? "" : <Required />}
         </div>
 
         {/* Short Answer */}
-        <div>
-          <h5>
-            Short Answer <Required />
-          </h5>
+        <div className="card">
+          <h6>
+            Short Answer <span className="text-danger">*</span>
+          </h6>
+
           <input type="text" placehoder="Your Answer" />
+          {isMCQ ? "" : <Required errorTxt="This is a required question" />}
         </div>
 
         {/* ParaGrap */}
-        <div>
-          <h5>
-            ParaGrap <Required />
-          </h5>
+        <div className="card">
+          <h6>
+            ParaGrap <span className="text-danger">*</span>
+          </h6>
           <input type="text" placehoder="Your Answer" />
+          {isMCQ ? "" : <Required errorTxt="This is a required question" />}
         </div>
 
         {/* Drop down */}
-        <div>
-          <h5>
-            Drop down <Required />
-          </h5>
-          <select>
+        <div className="card">
+          <h6>
+            Drop down <span className="text-danger">*</span>
+          </h6>
+          <select className="w-25 m-3 px-2 py-2 rounded-2 text-secondary border-secondary">
             <option>Choose</option>
             <hr />
             <option>Option 1</option>
             <option>Option 2</option>
             <option>Option 3</option>
           </select>
+          {isMCQ ? "" : <Required errorTxt="This is a required question" />}
         </div>
 
         {/* File Upload */}
-        <div>
-          <h5>
-            File Upload <Required />
-          </h5>
-          <input type="file" values="Add Value" />
+        <div className="card">
+          <h6>
+            File Upload <span className="text-danger">*</span>
+          </h6>
+          <label
+            htmlFor="file"
+            className="text-primary w-25 m-2 px-2 py-2 rounded-2 border-1 border border-secondary fw-bold "
+          >
+            <input
+              type="file"
+              value=""
+              id="file"
+              placeholder="Add Value"
+              style={{ width: "0px" }}
+              className=""
+            />
+            <BsUpload />
+            &nbsp; &nbsp; Add Value
+          </label>
+          {isMCQ ? "" : <Required errorTxt="This is a required question" />}
         </div>
 
         {/* Linear Scale */}
-        <table class="table caption-top">
-          <caption>
-            Linear Scale <Required />
-          </caption>
-          <thead>
-            <tr>
-              <th scope="col"> </th>
-              <th scope="col">1</th>
-              <th scope="col">2</th>
-              <th scope="col">3</th>
-              <th scope="col">4</th>
-              <th scope="col">5</th>
-              <th scope="col"> </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">Worst</th>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>best</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="card">
+          <table className="table caption-top">
+            <caption>
+              Linear Scale <span className="text-danger">*</span>
+            </caption>
+            <thead>
+              <tr>
+                <th scope="col"> </th>
+                <th scope="col">1</th>
+                <th scope="col">2</th>
+                <th scope="col">3</th>
+                <th scope="col">4</th>
+                <th scope="col">5</th>
+                <th scope="col"> </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Worst</th>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>best</td>
+              </tr>
+            </tbody>
+          </table>
+          {isMCQ ? "" : <Required errorTxt="This is a required question" />}
+        </div>
 
         {/* Multi choice Grid */}
-        <table class="table caption-top">
-          <caption>
-            Multi choice Grid <Required />
-          </caption>
-          <thead>
-            <tr>
-              <th scope="col"> </th>
-              <th scope="col">Column 1</th>
-              <th scope="col">Column 2</th>
-              <th scope="col">Column 3</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">Row 1</th>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>
-                <input type="radio" />
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Row 2</th>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>
-                <input type="radio" />
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Row 2</th>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>
-                <input type="radio" />
-              </td>
-              <td>
-                <input type="radio" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="card">
+          <table class="table caption-top">
+            <caption>
+              Multi choice Grid <span className="text-danger">*</span>
+            </caption>
+            <thead>
+              <tr>
+                <th scope="col"> </th>
+                <th scope="col">Column 1</th>
+                <th scope="col">Column 2</th>
+                <th scope="col">Column 3</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Row 1</th>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>
+                  <input type="radio" />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Row 2</th>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>
+                  <input type="radio" />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Row 2</th>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>
+                  <input type="radio" />
+                </td>
+                <td>
+                  <input type="radio" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          {isMCQ ? (
+            ""
+          ) : (
+            <Required errorTxt="This question requires at least one response per row" />
+          )}
+        </div>
 
         {/*Tick box Grid  */}
-        <table class="table caption-top">
-          <caption>
-            Tick box Grid <Required />
-          </caption>
-          <thead>
-            <tr>
-              <th scope="col"> </th>
-              <th scope="col">Column 1</th>
-              <th scope="col">Column 2</th>
-              <th scope="col">Column 3</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">Row 1</th>
-              <td>
-                <input type="checkbox" />
-              </td>
-              <td>
-                <input type="checkbox" />
-              </td>
-              <td>
-                <input type="checkbox" />
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Row 2</th>
-              <td>
-                <input type="checkbox" />
-              </td>
-              <td>
-                <input type="checkbox" />
-              </td>
-              <td>
-                <input type="checkbox" />
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">Row 2</th>
-              <td>
-                <input type="checkbox" />
-              </td>
-              <td>
-                <input type="checkbox" />
-              </td>
-              <td>
-                <input type="checkbox" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="card">
+          <table class="table caption-top">
+            <caption>
+              Tick box Grid <span className="text-danger">*</span>
+            </caption>
+            <thead>
+              <tr>
+                <th scope="col"> </th>
+                <th scope="col">Column 1</th>
+                <th scope="col">Column 2</th>
+                <th scope="col">Column 3</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Row 1</th>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>
+                  <input type="checkbox" />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Row 2</th>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>
+                  <input type="checkbox" />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Row 2</th>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>
+                  <input type="checkbox" />
+                </td>
+                <td>
+                  <input type="checkbox" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          {isMCQ ? (
+            ""
+          ) : (
+            <Required errorTxt="This question requires at least one response per row" />
+          )}
+        </div>
       </div>
     </div>
   );
